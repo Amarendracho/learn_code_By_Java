8c28163f478a9cdf8a6834a006c65181ea2bbe17
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-12 22:22:08 -0500

    add Testing.java

--- Testing.java
+++ Testing.java
@@ -1,12 +1,18 @@
 package codePractice;
 
 public class Testing {
+	
+	String name;
 
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 5; i++) {
 			System.out.print(i + " ");
 		}
+		
+		Testing test = new Testing();
+		test.name = "Checking";
+		System.out.print(test.name);
 
 	}
 }
