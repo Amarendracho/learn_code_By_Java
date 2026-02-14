7e25d03bdfe9ee50c7716c99677c78f541be4248
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 23:00:32 -0500

    update Palindrome.java

--- Strings/Easy_problems/Test.java
+++ Strings/Easy_problems/Test.java
@@ -8,6 +8,18 @@ public class Test {
 		// Base case
 		if(left >= right)
 			return 1;
+		
+		if(s.charAt(left) != s.charAt(right))
+			return 0;
+		
+		return pal_check(s, left+1, right -1);
+		
+	}
+	
+	// Function to check if a string is a palindrome
+	
+	public static void main(String[] args) {
+		
 	}
 
 }
