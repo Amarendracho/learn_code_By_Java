e90cb7ca04b841a2f7aa9319863bff11cc398868
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 20:32:01 -0500

    update Palindrome.java

--- Strings/Easy_problems/Palindrome.java
+++ Strings/Easy_problems/Palindrome.java
@@ -2,20 +2,7 @@ package codePractice.Strings.Easy_problems;
 
 public class Palindrome {
 
-	// Brute force solution
-//	public static boolean palindrome_Check(String name) {
-//
-//		if (name == null)
-//			return false;
-//
-//		String reverse = "";
-//
-//		for (int i = name.length() - 1; i >= 0; i--) {
-//			reverse += name.charAt(i);
-//		}
-//		return name.equals(reverse);
-//	}
-
+	// brute-force (slow due to string concat)
 	public boolean palindrome_Check(String name) {
 
 		if (name == null)
@@ -26,19 +13,32 @@ public class Palindrome {
 		for (int i = name.length() - 1; i >= 0; i--) {
 			reverse += name.charAt(i);
 		}
-		
+
 		return name.equals(reverse);
+
+	}
+	
+	// brute-force but better (String) — StringBuilder reverse
+	public static boolean pali_check(String str) {
+		
+		if(str == null) 
+			return false;
+		
+		String rev = new StringBuffer(str).reverse().toString();
+		
+		return str.equals(rev);
 
 	}
 
 	public static void main(String[] args) {
-
-		// System.out.println(Palindrome.palindrome_Check("java"));
 		Palindrome pal = new Palindrome();
-		
 		System.out.println(pal.palindrome_Check("chehc"));
+		// Time: O(n), Space: O(n)
 		
-
-	}
+		// using String buffer
+		System.out.println(pali_check("poop"));
+		
+		
+	} 
 
 }
