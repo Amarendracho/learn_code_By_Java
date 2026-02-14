2f8d85ba9f1494f1a08add73c61bbec00890bd9f
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 20:16:57 -0500

    update Palindrome.java

--- Strings/Easy_problems/Palindrome.java
+++ Strings/Easy_problems/Palindrome.java
@@ -3,22 +3,41 @@ package codePractice.Strings.Easy_problems;
 public class Palindrome {
 
 	// Brute force solution
-	public static boolean palindrome_Check(String name) {
+//	public static boolean palindrome_Check(String name) {
+//
+//		if (name == null)
+//			return false;
+//
+//		String reverse = "";
+//
+//		for (int i = name.length() - 1; i >= 0; i--) {
+//			reverse += name.charAt(i);
+//		}
+//		return name.equals(reverse);
+//	}
+
+	public boolean palindrome_Check(String name) {
 
 		if (name == null)
 			return false;
 
 		String reverse = "";
 
-		for (int i = name.length() - 1; i >= 0; i--) {
+		for (int i = name.length() - 1; i <= 0; i--) {
 			reverse += name.charAt(i);
 		}
+		
 		return name.equals(reverse);
+
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println(Palindrome.palindrome_Check("java"));
+		// System.out.println(Palindrome.palindrome_Check("java"));
+		Palindrome pal = new Palindrome();
+		
+		System.out.println(pal.palindrome_Check("check"));
+		
 
 	}
 
