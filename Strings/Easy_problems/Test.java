b8d745c853868d42b247d2aac1dd59b508b7de4d
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 23:04:45 -0500

    add Test

--- Strings/Easy_problems/Test.java
+++ Strings/Easy_problems/Test.java
@@ -3,31 +3,8 @@ package codePractice.Strings.Easy_problems;
 public class Test {
 	
 	
-	static int pal_check(String s, int left, int right) {
-		
-		// Base case
-		if(left >= right)
-			return 1;
-		
-		if(s.charAt(left) != s.charAt(right))
-			return 0;
-		
-		return pal_check(s, left+1, right -1);
-		
-	}
-	
-	// Function to check if a string is a palindrome
-	static int isPal(String s) {
-		int left = 0;
-		int right = s.length()-1;
-		return pal_check(s, left, right);
-	}
-	
-	public static void main(String[] args) {
-		
-        String s = "abba";
-        System.out.println(isPal(s));
-		
-	}
 
+public static void main(String[] args) {
+	
+}
 }
