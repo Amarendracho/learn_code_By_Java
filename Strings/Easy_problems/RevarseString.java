482dd08969f6e3eb3818bfc76be6bd845c583eb3
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 23:13:13 -0500

    update RevarseString.java

--- Strings/Easy_problems/RevarseString.java
+++ Strings/Easy_problems/RevarseString.java
@@ -5,13 +5,19 @@ public class RevarseString {
 	// Using backward traversal – O(n) Time and O(n) Space
 	static String revarseStr(String s) {
 
-		for (int i = s.length() - 1; i >= 0; i--) {
+		String r = "";
 
+		for (int i = s.length() - 1; i >= 0; i--) {
+			r += s.charAt(i);
 		}
+
+		return r;
 
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println(revarseStr("amar"));
 
 //		Using Two Pointers - O(n) Time and O(1) Space
 //		Using Recursion - O(n) Time and O(n) Space
