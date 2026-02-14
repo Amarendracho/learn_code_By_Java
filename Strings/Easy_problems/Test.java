8d152e3fcb2f0b9ca282f2e2fa532177db75d7f2
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 22:58:02 -0500

    update Palindrome.java

---
+++ Strings/Easy_problems/Test.java
@@ -0,0 +1,13 @@
+package codePractice.Strings.Easy_problems;
+
+public class Test {
+	
+	
+	static int pal_check(String s, int left, int right) {
+		
+		// Base case
+		if(left >= right)
+			return 1;
+	}
+
+}
