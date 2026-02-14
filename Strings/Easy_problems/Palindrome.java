e24249f2fc67d17bb5e3b32a5a522fdd6267c96f
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-13 23:04:12 -0500

    update Palindrome.java

--- Strings/Easy_problems/Palindrome.java
+++ Strings/Easy_problems/Palindrome.java
@@ -78,6 +78,29 @@ public class Palindrome {
 		}
 		return 1;
 	}
+	
+	
+	// recursion 
+	static int pal_check(String s, int left, int right) {
+		
+		// Base case
+		if(left >= right)
+			return 1;
+		
+		if(s.charAt(left) != s.charAt(right))
+			return 0;
+		
+		return pal_check(s, left+1, right -1);
+		
+	}
+	
+	// Function to check if a string is a palindrome
+	static int isPal(String s) {
+		int left = 0;
+		int right = s.length()-1;
+		return pal_check(s, left, right);
+	}
+
 
 	public static void main(String[] args) {
 		Palindrome pal = new Palindrome();
