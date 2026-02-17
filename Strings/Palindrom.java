a4715b2a8e190591aa239270bd5e4e36373d038c
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-17 16:57:12 -0500

    update pali.py

--- Strings/Palindrom.java
+++ Strings/Palindrom.java
@@ -2,25 +2,30 @@ package codePractice.Strings;
 
 public class Palindrom {
 
-	public static boolean pal_Check(String str) {
-		if(str == null)
-			return false;
+	public static void pal_Check(String str){
+		try {
+			if(str == null)
+			System.err.println("input can't be null");
+		}
+		catch (NullPointerException e){
+			System.out.println(e.getStackTrace());
+		}
 		
 		int left = 0;
 		int right = str.length() - 1;
 
 		while (left < right) {
 			if (str.charAt(left) != str.charAt(right)) 
-				return false;
+				System.out.println("NOT - PALI");
 
 			left++;
 			right--;
 		}
-		return true;
+		System.out.println("PALI");
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println(Palindrom.pal_Check("josoj"));
+		Palindrom.pal_Check(null);
 	}
 } 
