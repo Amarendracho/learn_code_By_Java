5b25c31b7b4593fa0bc90f3e951e6067baa244fe
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-17 16:35:38 -0500

    add pali.py

--- Strings/Palindrom.java
+++ Strings/Palindrom.java
@@ -2,20 +2,18 @@ package codePractice.Strings;
 
 public class Palindrom {
 
-	public static String pal_Check(String str) {
+	public static boolean pal_Check(String str) {
 
 		String revarse = "";
 
 		for (int i = str.length() - 1; i >= 0; i--) {
 			revarse += str.charAt(i);
 		}
-		if (!str.equals(revarse))
-			return "NOT PALINDROM";
 
-		return "PALINDROM";
+		return str.equals(revarse);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(Palindrom.pal_Check("NANNA")); 
+		System.out.println(Palindrom.pal_Check("dad")); 
 	}
 }
