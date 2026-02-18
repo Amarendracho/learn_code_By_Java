1248761f987d9989f5b86532f7f68bbe79771674
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-17 22:04:45 -0500

    update ConvertLowerToUpper.java

--- Strings/BasicProblems/ConvertLowerToUpper.java
+++ Strings/BasicProblems/ConvertLowerToUpper.java
@@ -1,7 +1,21 @@
 package codePractice.Strings.BasicProblems;
 
+import java.util.Scanner;
+
 // Convert Lowercase to Uppercase (without using toUpperCase())
 
 public class ConvertLowerToUpper {
+	
+	// with toUpperCase()
+	static void toUpper() {
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("Enter a String");
+			String toUpper= scan.nextLine().toUpperCase();
+			System.out.println(toUpper);
+		}
+	}
 
+	public static void main(String[] args) {
+		toUpper();
+	}
 }
