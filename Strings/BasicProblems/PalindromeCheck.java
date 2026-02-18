5fee1e4786dd780a7b132d09c424c6d68a0c1ecb
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-17 19:36:37 -0500

    update PalindromeCheck.java

--- Strings/BasicProblems/PalindromeCheck.java
+++ Strings/BasicProblems/PalindromeCheck.java
@@ -1,19 +1,46 @@
 package codePractice.Strings.BasicProblems;
 
+import java.util.Scanner;
+
 public class PalindromeCheck {
-	
+
 	public static String palCheck(String str) {
-		
+
 		String rev = "";
-		for(int i = str.length()-1; i>= 0; i--) {
+		for (int i = str.length() - 1; i >= 0; i--) {
 			rev += str.charAt(i);
-		} 
-		if(!str.equals(rev))
+		}
+		if (!str.equals(rev))
 			return "NOT PALINDROME";
-		
+
 		return "PALINDROME";
 	}
+
+	// taking input
+	static String palindromeCheck() {
+
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.print("Enter a string: ");
+			String input = scan.nextLine();
+
+			int left = 0;
+			int right = input.length() - 1;
+
+			while (left < right) {
+				if (input.charAt(left) != input.charAt(right))
+					return "NOT PALINDROME";
+
+				left++;
+				right--;
+
+			}
+			return "PALINDROME";
+
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println(PalindromeCheck.palCheck("jssuj"));
+		System.out.println(palindromeCheck());
 	}
 }
